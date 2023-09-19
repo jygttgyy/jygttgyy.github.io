6e38d4f946e4211489377d5ce888a5d010fcfa4b
jygttgyy <92969468+jygttgyy@users.noreply.github.com>
2023-09-19 09:06:31 +0200

    :D

--- new.js
+++ new.js
@@ -2,9 +2,35 @@ var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
 
 window.onscroll = function() {
-    if (window.pageYOffset > sticky) {
-      header.classList.add("sticky");
-    } else {
-      header.classList.remove("sticky");
-    }
+	if (window.pageYOffset > sticky) {
+		header.classList.add("sticky");
+	} else {
+		header.classList.remove("sticky");
+	}
+}
+
+var mainContent = document.getElementById("mainContent");
+var delta_devContentContent = document.getElementById("delta_devContent");
+var bearContent = document.getElementById("bearContent");
+
+document.getElementById("mainButton").onclick = function() {
+	if (mainContent.style.display === "none") {
+		delta_devContent.style.display = "none";
+		bearContent.style.display = "none";
+		mainContent.style.display = "block";
+	}
+}
+document.getElementById("delta_devButton").onclick = function() {
+	if (delta_devContent.style.display === "none") {
+		mainContent.style.display = "none";
+		bearContent.style.display = "none";
+		delta_devContent.style.display = "block";
+	}
+}
+document.getElementById("bearButton").onclick = function() {
+	if (bearContent.style.display === "none") {
+		mainContent.style.display = "none";
+		delta_devContent.style.display = "none";
+		bearContent.style.display = "block";
+	}
 }
