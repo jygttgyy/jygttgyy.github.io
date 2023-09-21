6f2335b989e358262d4ccf54e98fdb639f21d137
jygttgyy <92969468+jygttgyy@users.noreply.github.com>
2023-09-21 06:10:44 +0200

    :D

--- new.js
+++ new.js
@@ -16,19 +16,19 @@ document.getElementById("discordButton").onclick = function() {
 var mainContent = document.getElementById("mainContent");
 var delta_devContent = document.getElementById("delta_devContent");
 var bearContent = document.getElementById("bearContent");
-document.getElementById("mainButton").onclick = function openMain() {
+document.getElementById("mainButton").onclick = function() {
 		console.log("mainButton: Clicked");
 		delta_devContent.style.display = "none";
 		bearContent.style.display = "none";
 		mainContent.style.display = "block";
 }
-document.getElementById("delta_devButton").clicked = function openDelta_Dev() {
+document.getElementById("delta_devButton").onclick = function() {
 		console.log("delta_dev_Button: Clicked");
 		mainContent.style.display = "none";
 		bearContent.style.display = "none";
 		delta_devContent.style.display = "block";
 }
-document.getElementById("bearButton").onclick = function openBear() {
+document.getElementById("bearButton").onclick = function() {
 		console.log("bearButton: Clicked");
 		mainContent.style.display = "none";
 		delta_devContent.style.display = "none";
