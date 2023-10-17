84ad02058c9aaab713a9ae7dc6436d217bd5be26
jygttgyy <92969468+jygttgyy@users.noreply.github.com>
2023-10-17 14:58:15 +0200

    :D

--- new.js
+++ new.js
@@ -2,7 +2,7 @@ var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
 
 window.onscroll = function() {
-	if (window.pageYOffset > sticky) {
+	if (window.pageYOffset > sticky || window.scrollY > sticky) {
 		header.classList.add("sticky");
 	} else {
 		header.classList.remove("sticky");
@@ -17,20 +17,20 @@ var mainContent = document.getElementById("mainContent");
 var delta_devContent = document.getElementById("delta_devContent");
 var bearContent = document.getElementById("bearContent");
 document.getElementById("mainButton").onclick = function() {
-		console.log("mainButton: Clicked");
-		delta_devContent.style.display = "none";
-		bearContent.style.display = "none";
-		mainContent.style.display = "block";
+	console.log("mainButton: Clicked");
+	delta_devContent.style.display = "none";
+	bearContent.style.display = "none";
+	mainContent.style.display = "block";
 }
 document.getElementById("delta_devButton").onclick = function() {
-		console.log("delta_dev_Button: Clicked");
-		mainContent.style.display = "none";
-		bearContent.style.display = "none";
-		delta_devContent.style.display = "block";
+	console.log("delta_dev_Button: Clicked");
+	mainContent.style.display = "none";
+	bearContent.style.display = "none";
+	delta_devContent.style.display = "block";
 }
 document.getElementById("bearButton").onclick = function() {
-		console.log("bearButton: Clicked");
-		mainContent.style.display = "none";
-		delta_devContent.style.display = "none";
-		bearContent.style.display = "block";
+	console.log("bearButton: Clicked");
+	mainContent.style.display = "none";
+	delta_devContent.style.display = "none";
+	bearContent.style.display = "block";
 }
