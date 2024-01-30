2543d748fe82de7c2c450af1491ac9742910d694
jygttgyy <92969468+jygttgyy@users.noreply.github.com>
2024-01-30 14:52:52 +0100

    e

--- projects/currentDate/script.js
+++ projects/currentDate/script.js
@@ -7,7 +7,7 @@ function num0(num) {
 }
 function e() {
     var d = new Date()
-    document.getElementById("date").innerText = num0(d.getHours()) + ":" + num0(d.getMinutes()) + ":" + num0(d.getSeconds()) + "\n" + d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear();
+    document.getElementById("date").innerText = num0(d.getHours()) + ":" + num0(d.getMinutes()) + ":" + num0(d.getSeconds()) + "\n" + d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear();
     setTimeout(e, 1);
 }
 e();
